58d6ed0dbde8911d89af3649b81790c941a9adf1
Nyxu <nyxuofficial@gmail.com>
2026-06-22 21:01:42 -0400

    feat: inicio del proyecto con Gradle y las dependencias iniciales requeridas

---
+++ backend/reservas-backend/src/test/java/cl/ucm/hotel/reservas_backend/ReservasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ucm.hotel.reservas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
